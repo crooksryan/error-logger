faed342781a0126a8d1e286ba08e1a19b93e3049
ryan crooks <dawson4623@gmail.com>
2024-05-24 12:19:42 -0400

    swapped udp to tcp

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ mod structs;
 use crate::structs::*;
 use simple_logger;
 use log::{set_max_level, info, LevelFilter};
-use std::{time::Duration, net::{UdpSocket,SocketAddr}};
+use std::net::SocketAddr;
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{TcpListener, TcpStream}};
 
-fn handle_message(buf: Vec<u8>, amt: usize, src: SocketAddr, socket: UdpSocket) -> std::io::Result<()> {
-    let decode = String::from_utf8(buf[..amt].to_vec()).unwrap();
+async fn handle_message(stream: &mut TcpStream, src: &SocketAddr) -> std::io::Result<()> {
+    let mut buf = String::new();
+    let _n = stream.read_to_string(&mut buf).await?;
 
-    let deserialized: Result<Message, serde_json::Error> = serde_json::from_str(&decode);
+    let deserialized: Result<Message, serde_json::Error> = serde_json::from_str(&buf);
 
     if let Ok(deserialized) = deserialized {
         info!("recieved from {}: {} [{}]", src, deserialized.message, deserialized.code);
@@ -16,60 +18,72 @@ fn handle_message(buf: Vec<u8>, amt: usize, src: SocketAddr, socket: UdpSocket) 
         let response = serde_json::to_string(&response).unwrap();
         let response = response.as_bytes();
 
-        let _bytes = socket.send_to(response, src)?;
+        // let _bytes = socket.send_to(response, src)?;
+        stream.write(response).await?;
 
         Ok(())
     } else {
+        info!("Src {}: couldn't deserialize", src);
         Err(std::io::Error::new(std::io::ErrorKind::Other, "broke"))
     }
 }
 
-fn simple_handle(buf: Vec<u8>, amt: usize, src: SocketAddr, socket: UdpSocket) {
-    let decode = String::from_utf8(buf[..amt].to_vec()).unwrap();
+async fn simple_handle(stream: &mut TcpStream, src: &SocketAddr) -> std::io::Result<()> {
+    let mut buf = String::new();
+    let _n = stream.read_to_string(&mut buf).await?;
 
-    info!("recieved simple from {}: {}", src, decode);
+    info!("recieved simple from {}: {}", src, buf);
 
     let response = Response { code: 200 };
     let response = serde_json::to_string(&response);
 
     if let Ok(response) = response {
         let response = response.as_bytes();
-        let _result_bytes = socket.send_to(response, src);
+        let _result_bytes = stream.write(response).await?;
     } else {
         info!("couldn't encode res")
     }
+
+    Ok(())
 }
 
-fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
     simple_logger::init().unwrap();
     set_max_level(LevelFilter::Info);
 
     info!("Start Up on port 8000");
 
-    let socket = UdpSocket::bind("127.0.0.1:8000")?;
-    socket.set_read_timeout(Some(Duration::from_millis(15))).unwrap();
-    socket.set_write_timeout(Some(Duration::from_millis(15))).unwrap();
-    socket.set_nonblocking(true)?;
+    let listener = TcpListener::bind("127.0.0.1:8000").await?;
 
-    info!("Listening");
+    info!("Listening...");
 
     loop {
-        let mut buf = vec![0; 50];
-        let res = socket.recv_from(&mut buf);
+        if let Ok((mut stream, addr)) = listener.accept().await {
+            tokio::spawn(async move {
+                let mut structured = 0;
+                loop {
+                    if structured == 1 {
+                        let res = handle_message(&mut stream, &addr).await;
 
-        let (amt, src) = match res {
-            Ok(res) => (res.0, res.1),
-            Err(_) => continue
-        };
-        
-        let sock2 = socket.try_clone()?;
+                        if let Err(_) = res {
+                            structured = 1
+                        }
+                    } else {
+                        let res = simple_handle(&mut stream, &addr).await;
 
-        match handle_message(buf.clone(), amt, src, sock2) {
-            Ok(_) => {},
-            Err(_) => {
-                info!("connection required simple handle");
-                simple_handle(buf.clone(), amt, src, socket.try_clone()?);
-            }
+                        if let Err(_) = res {
+                            info!("Ending connection with: {}", addr);
+                            break;
+                        }
+                    }
+                }
+            });
+
+
+        } else {
+            info!("Error when accepting connection")
         }
     }
+
 }
